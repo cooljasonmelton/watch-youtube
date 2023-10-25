b1ea288b44820e2e77fd6b3274d292401deba402
Jason Melton <jason.melton@netadmins-MBP.attlocal.net>
2023-10-25 11:54:30 -0500

    update styles and functionality add clear button

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-let watchYoutubeForm = document.getElementById('watch-youtube-form')
+let watchYoutubeForm = document.getElementById("watch-youtube-form");
+let flinkInput = document.getElementById("flink");
 
-watchYoutubeForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let flinkInputValue = document.getElementById('flink').value
-  openVideoOnly(flinkInputValue)
-})
+watchYoutubeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let flinkInputValue = flinkInput.value;
+  openVideoOnly(flinkInputValue);
+});
 
 // open YouTube links as embedded video in new tab
-const openVideoOnly = (url = '') => {
+const openVideoOnly = (url = "") => {
   try {
-    let videoId = ''
-    const formattedUrl = url.split('&')[0]
-    if (url.includes('v=')) {
-      videoId = formattedUrl.split('v=')[1]
+    let videoId = "";
+    const formattedUrl = url.split("&")[0];
+    if (url.includes("v=")) {
+      videoId = formattedUrl.split("v=")[1];
     }
-    if (url.includes('youtu.be')) {
-      const formattedUrlParams = formattedUrl.split('youtu.be')[1]
-      videoId = formattedUrlParams.split('?')[0]
+    if (url.includes("youtu.be")) {
+      const formattedUrlParams = formattedUrl.split("youtu.be/")[1];
+      videoId = formattedUrlParams.split("?")[0];
     }
+    console.log(videoId);
+    if (videoId.length === 11) {
+      window.open(`https://www.youtube.com/embed/${videoId}`, "_blank");
+    } else throw new Error();
 
-    window.open(`https://www.youtube.com/embed/${videoId}`, '_blank')
+    // window.open(`https://www.youtube.com/embed/${videoId}`, "_blank");
   } catch (e) {
-    alert('didnt work, probably bad url')
+    alert("didnt work, probably bad url");
   }
-}
+};
+
+const clearForm = () => (flinkInput.value = "");
